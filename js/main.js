46ed75c036482ef458b0698400e0fda6f1e4a442
Matteo Zamaria <matteozamaria@gmail.com>
2020-10-01 17:47:53 -0400

    Remove unecessary file, fix formatting.

--- js/main.js
+++ js/main.js
@@ -84,62 +84,62 @@ function ProcessSequence() {
     });
 
     //Get NT totals
-        // A //
-        document.querySelector("#amountDNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "d").length;
-        document.querySelector("#amountRNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "r").length;
-        document.querySelector("#amountFluoro_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "f").length;
-        document.querySelector("#amountLNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "l").length;
-        document.querySelector("#amountOMe_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "m").length;
-        document.querySelector("#amountFANA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "a").length;
-        document.querySelector("#amountANA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "ara").length;
-        document.querySelector("#amountTotal_A").innerHTML = ntArr.filter(n => n.base ==="A").length;
-        // T //
-        document.querySelector("#amountDNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "d").length;
-        document.querySelector("#amountRNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "r").length;
-        document.querySelector("#amountFluoro_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "f").length;
-        document.querySelector("#amountLNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "l").length;
-        document.querySelector("#amountOMe_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "m").length;
-        document.querySelector("#amountFANA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "a").length;
-        document.querySelector("#amountANA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "ara").length;
-        document.querySelector("#amountTotal_T").innerHTML = ntArr.filter(n => n.base ==="T").length;
-        // G //
-        document.querySelector("#amountDNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "d").length;
-        document.querySelector("#amountRNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "r").length;
-        document.querySelector("#amountFluoro_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "f").length;
-        document.querySelector("#amountLNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "l").length;
-        document.querySelector("#amountOMe_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "m").length;
-        document.querySelector("#amountFANA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "a").length;
-        document.querySelector("#amountANA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "ara").length;
-        document.querySelector("#amountTotal_G").innerHTML = ntArr.filter(n => n.base ==="G").length;
-        // C //
-        document.querySelector("#amountDNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "d").length;
-        document.querySelector("#amountRNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "r").length;
-        document.querySelector("#amountFluoro_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "f").length;
-        document.querySelector("#amountLNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "l").length;
-        document.querySelector("#amountOMe_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "m").length;
-        document.querySelector("#amountFANA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "a").length;
-        document.querySelector("#amountANA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "ara").length;
-        document.querySelector("#amountTotal_C").innerHTML = ntArr.filter(n => n.base ==="C").length;
-        // U //
-        document.querySelector("#amountDNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "d").length;
-        document.querySelector("#amountRNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "r").length;
-        document.querySelector("#amountFluoro_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "f").length;
-        document.querySelector("#amountLNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "l").length;
-        document.querySelector("#amountOMe_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "m").length;
-        document.querySelector("#amountFANA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "a").length;
-        document.querySelector("#amountANA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "ara").length;
-        document.querySelector("#amountTotal_U").innerHTML = ntArr.filter(n => n.base ==="U").length;
-        // TOTALS //
-        document.querySelector("#amountDNA_Total").innerHTML = ntArr.filter(n => n.type === "d").length;
-        document.querySelector("#amountRNA_Total").innerHTML = ntArr.filter(n => n.type === "r").length;
-        document.querySelector("#amountFluoro_Total").innerHTML = ntArr.filter(n => n.type === "f").length;
-        document.querySelector("#amountLNA_Total").innerHTML = ntArr.filter(n => n.type === "l").length;
-        document.querySelector("#amountOMe_Total").innerHTML = ntArr.filter(n => n.type === "m").length;
-        document.querySelector("#amountFANA_Total").innerHTML = ntArr.filter(n => n.type === "a").length;
-        document.querySelector("#amountANA_Total").innerHTML = ntArr.filter(n => n.type === "ara").length;
-        document.querySelector("#amountTotal_Total").innerHTML = ntArr.filter(n => n != "#").length;
-        // # //
-        document.querySelector("#amountTotal_Hash").innerHTML = ntArr.filter(n => n ==="#").length;
+    // A //
+    document.querySelector("#amountDNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "d").length;
+    document.querySelector("#amountRNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "r").length;
+    document.querySelector("#amountFluoro_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "f").length;
+    document.querySelector("#amountLNA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "l").length;
+    document.querySelector("#amountOMe_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "m").length;
+    document.querySelector("#amountFANA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "a").length;
+    document.querySelector("#amountANA_A").innerHTML = ntArr.filter(n => n.base ==="A" && n.type === "ara").length;
+    document.querySelector("#amountTotal_A").innerHTML = ntArr.filter(n => n.base ==="A").length;
+    // T //
+    document.querySelector("#amountDNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "d").length;
+    document.querySelector("#amountRNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "r").length;
+    document.querySelector("#amountFluoro_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "f").length;
+    document.querySelector("#amountLNA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "l").length;
+    document.querySelector("#amountOMe_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "m").length;
+    document.querySelector("#amountFANA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "a").length;
+    document.querySelector("#amountANA_T").innerHTML = ntArr.filter(n => n.base ==="T" && n.type === "ara").length;
+    document.querySelector("#amountTotal_T").innerHTML = ntArr.filter(n => n.base ==="T").length;
+    // G //
+    document.querySelector("#amountDNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "d").length;
+    document.querySelector("#amountRNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "r").length;
+    document.querySelector("#amountFluoro_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "f").length;
+    document.querySelector("#amountLNA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "l").length;
+    document.querySelector("#amountOMe_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "m").length;
+    document.querySelector("#amountFANA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "a").length;
+    document.querySelector("#amountANA_G").innerHTML = ntArr.filter(n => n.base ==="G" && n.type === "ara").length;
+    document.querySelector("#amountTotal_G").innerHTML = ntArr.filter(n => n.base ==="G").length;
+    // C //
+    document.querySelector("#amountDNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "d").length;
+    document.querySelector("#amountRNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "r").length;
+    document.querySelector("#amountFluoro_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "f").length;
+    document.querySelector("#amountLNA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "l").length;
+    document.querySelector("#amountOMe_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "m").length;
+    document.querySelector("#amountFANA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "a").length;
+    document.querySelector("#amountANA_C").innerHTML = ntArr.filter(n => n.base ==="C" && n.type === "ara").length;
+    document.querySelector("#amountTotal_C").innerHTML = ntArr.filter(n => n.base ==="C").length;
+    // U //
+    document.querySelector("#amountDNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "d").length;
+    document.querySelector("#amountRNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "r").length;
+    document.querySelector("#amountFluoro_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "f").length;
+    document.querySelector("#amountLNA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "l").length;
+    document.querySelector("#amountOMe_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "m").length;
+    document.querySelector("#amountFANA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "a").length;
+    document.querySelector("#amountANA_U").innerHTML = ntArr.filter(n => n.base ==="U" && n.type === "ara").length;
+    document.querySelector("#amountTotal_U").innerHTML = ntArr.filter(n => n.base ==="U").length;
+    // TOTALS //
+    document.querySelector("#amountDNA_Total").innerHTML = ntArr.filter(n => n.type === "d").length;
+    document.querySelector("#amountRNA_Total").innerHTML = ntArr.filter(n => n.type === "r").length;
+    document.querySelector("#amountFluoro_Total").innerHTML = ntArr.filter(n => n.type === "f").length;
+    document.querySelector("#amountLNA_Total").innerHTML = ntArr.filter(n => n.type === "l").length;
+    document.querySelector("#amountOMe_Total").innerHTML = ntArr.filter(n => n.type === "m").length;
+    document.querySelector("#amountFANA_Total").innerHTML = ntArr.filter(n => n.type === "a").length;
+    document.querySelector("#amountANA_Total").innerHTML = ntArr.filter(n => n.type === "ara").length;
+    document.querySelector("#amountTotal_Total").innerHTML = ntArr.filter(n => n != "#").length;
+    // # //
+    document.querySelector("#amountTotal_Hash").innerHTML = ntArr.filter(n => n ==="#").length;
 }
 
  function SelectText(_containerid) {
